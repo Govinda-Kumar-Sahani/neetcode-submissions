91ee1ca7205a2dc758fad03b14ceab6943ccea2d
Govinda Kumar Sahani <govindakumarsahani482@gmail.com>
2026-05-31 12:15:58 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        """sorted_str1=sorted(s)
+        sorted_str2=sorted(t)
+        if sorted_str1==sorted_str2:
+            return True
+        else:
+            return False"""
+        if len(s)!=len(t):
+            return False
+        dict1,dict2={},{}
+        for i in range(len(s)):
+            dict1[s[i]]=1+dict1.get(s[i],0)
+            dict2[t[i]]=1+dict2.get(t[i],0)
+        for ch in dict1:
+            if dict1[ch]!=dict2.get(ch,0):
+                return False
+        return True
+    
+        
